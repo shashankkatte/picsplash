99ff8391d1f09aedd2e13271b3d6fe5dd63fd9ad
shashankkatte <shashankanand.katte@gmail.com>
2020-10-13 00:49:19 -0400

    Using async await for Axios network call

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID P1LUafb9NFVPRiB4_NHUk6dtzl5DOn_4HFijQP4sSxs',
       },
     });
+    console.log(response.data.results);
   }
 
   render() {
